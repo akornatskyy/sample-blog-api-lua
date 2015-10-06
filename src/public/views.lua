d529bf494a1b1137e462ff397f5dc170184f969e
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-06 20:45:31 +0300

    Added daily quote public web handler.

---
+++ src/public/views.lua
@@ -0,0 +1,23 @@
+local class = require 'core.class'
+
+local shared = require 'shared.views'
+
+
+local DailyQuoteHandler = class(shared.BaseHandler, {
+    get = function(self)
+        return self:json(self:get_daily_quote())
+    end,
+
+    get_daily_quote = function(self)
+        return self:factory('ro', function(f)
+            return f.quote:daily()
+        end)
+    end
+})
+
+
+-- url mapping
+
+return {
+    {'quote/daily', DailyQuoteHandler}
+}
