12342e34c865b81774b4b959b127d08143774184
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-07 16:57:07 +0300

    Added loaders for repository and service to utils.

--- src/shared/utils.lua
+++ src/shared/utils.lua
@@ -1,18 +1,49 @@
-local function merge(t, s)
-    for _, d in ipairs(s) do
-        t[#t+1] = d
+local function extend(t, s)
+    for i = 1, #s do
+        t[#t+1] = s[i]
     end
     return t
+end
+
+local function load_repositories(sessions, mode, names)
+    local r = {}
+    mode = '.repository.' .. mode
+    for session_name, session in pairs(sessions) do
+        r[session_name] = {}
+        for i = 1, #names do
+            local module = require(names[i] .. mode)
+            for name, metaclass in pairs(module) do
+                r[session_name][name] = setmetatable(
+                    {session=sessions[session_name]},
+                    {__index = metaclass}
+                )
+            end
+        end
+    end
+    return r
+end
+
+local function load_services(names)
+    local s = {}
+    for i = 1, #names do
+        local module = require(names[i] .. '.service')
+        for name, metaclass in pairs(module) do
+            s[name] = {__index = metaclass}
+        end
+    end
+    return s
 end
 
 local function load_urls(views)
     local urls = {}
     for i = 1, #views do
-        merge(urls, require(views[i] .. '.views'))
+        extend(urls, require(views[i] .. '.views'))
     end
     return urls
 end
 
 return {
+    load_repositories = load_repositories,
+    load_services = load_services,
     load_urls = load_urls
 }
