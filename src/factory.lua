9c95b694f5525ba32fde67045e7621151f2952ab
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-07 18:34:03 +0300

    Refactored factory module to use repository and service loaders from utils.

--- src/factory.lua
+++ src/factory.lua
@@ -1,3 +1,4 @@
+local _ = require 'shared.utils'
 local mode = os.getenv('mode') or 'mock'
 
 
@@ -7,32 +8,14 @@ local sessions = {
 }
 
 -- repository lifetime: singleton
-local repositories
-do
-    local public = require('public.repository.' .. mode)
-
-    local function mk(session_name, metaclass)
-        return setmetatable({session=sessions[session_name]},
-                            {__index = metaclass})
-    end
-
-    repositories = {
-        ro = {
-            quote = mk('ro', public.quote)
-        }
-    }
-end
+local repositories = _.load_repositories(sessions, mode, {'public'})
 
 -- service lifetime: per request
-local services = {
-    quote = {__index = require 'public.service'}
-}
+local services = _.load_services {'public'}
 
 local Factory = {
     __index = function(self, name)
-        local s = setmetatable({
-            factory = repositories[self.session_name]
-        }, services[name])
+        local s = setmetatable({factory = self.factory}, services[name])
         self[name] = s
         return s
     end
@@ -40,5 +23,6 @@ local Factory = {
 
 return function(self, func)
     -- TODO: unit of work scope
+    self.factory = repositories[self.session_name]
     return func(setmetatable(self, Factory))
 end
