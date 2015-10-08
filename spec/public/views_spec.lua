47fca1fc7627827013f176a79fa1e6a2b1ae3667
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-08 19:03:26 +0300

    Refined daily quote functional test case.

--- spec/public/views_spec.lua
+++ spec/public/views_spec.lua
@@ -1,4 +1,5 @@
 local client = require 'shared.client'
+local json = require 'core.encoding.json'
 local describe, it, assert = describe, it, assert
 
 
@@ -6,9 +7,14 @@ describe('public.views', function()
     local go, path_for = client.new()
 
     describe('daily quote', function()
+        local path = path_for('daily-quote')
+
     	it('responds with a quote', function()
-            local w = go({path = path_for('daily-quote')})
+            local w = go {path = path}
             assert.is_nil(w.status_code)
+            local q = json.decode(table.concat(w.buffer))
+            assert(q.author)
+            assert(q.message)
         end)
     end)
 end)
