1d99ca0e1efcf5ad7ead5c428a3a09a08ec4e207
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-08 11:48:02 +0300

    Added several utility functions to shared mock module.

--- src/shared/mock.lua
+++ src/shared/mock.lua
@@ -1,5 +1,72 @@
 local json = require 'core.encoding.json'
 
+local function map(t, f, s, e)
+    if not s then s = 1 end
+    if not e then e = #t end
+    local r = {}
+    for i = s, e do
+        r[#r+1] = f(t[i])
+    end
+    return r
+end
+
+local function trancate_words(s, count)
+    local n = count
+    local r = {}
+    s = s:gsub('\\n', ' ')
+    for i in s:gmatch('%S+') do
+        if n == 0 then
+            r[#r+1] = '...'
+            break
+        end
+        r[#r+1] = i
+        n = n - 1
+    end
+    return table.concat(r, ' ')
+end
+
+local function pager(items, page, size, f)
+    local s = page * size + 1
+    local e = s + size - 1
+    local n = #items
+    local paging = {}
+    if page > 0 then
+        paging.before = page - 1
+    end
+    if e < n then
+        paging.after = page + 1
+    else
+        e = n
+    end
+    return {
+        paging = paging,
+        items = map(items, f, s, e)
+    }
+end
+
+local function first(items, predicate)
+    for i = 1, #items do
+        local d = items[i]
+        if predicate(d) then
+            return d
+        end
+    end
+end
+
+local function nfilter(items, n, predicate)
+    local r = {}
+    for i = 1, #items do
+        local d = items[i]
+        if predicate(d) then
+            r[#r+1] = d
+            n = n -1
+            if n == 0 then
+                break
+            end
+        end
+    end
+    return r
+end
 
 local function load_samples()
     local info = debug.getinfo(2, 'S')
@@ -11,5 +78,9 @@ local function load_samples()
 end
 
 return {
+    trancate_words = trancate_words,
+    pager = pager,
+    first = first,
+    nfilter = nfilter,
     load_samples = load_samples
 }
