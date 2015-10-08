578626377b502c29613068ee5fc1327177b4714a
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-08 15:04:22 +0300

    Added functional test cases for search and get post.

--- spec/posts/views_spec.lua
+++ spec/posts/views_spec.lua
@@ -1,4 +1,5 @@
 local client = require 'shared.client'
+local json = require 'core.encoding.json'
 local describe, it, assert = describe, it, assert
 
 
@@ -6,9 +7,77 @@ describe('posts.views', function()
     local go, path_for = client.new()
 
     describe('search posts', function()
-    	it('responds with a quote', function()
-            local w = go({path = path_for('search-posts')})
+        local path = path_for('search-posts')
+
+    	it('validates query params', function()
+            local w = go {path = path, query = {page = 'x'}}
+            assert.equals(400, w.status_code)
+            assert.same({['Content-Type'] = 'application/json'}, w.headers)
+            local errors = json.decode(table.concat(w.buffer))
+            assert(errors.page)
+        end)
+
+    	it('responds with a list of posts', function()
+            local w = go {path = path}
             assert.is_nil(w.status_code)
+            assert.same({['Content-Type'] = 'application/json'}, w.headers)
+            local posts = json.decode(table.concat(w.buffer))
+            assert.equals(2, #posts.items)
+            assert.equals(1, posts.paging.after)
+        end)
+
+        it('supports paging', function()
+            local w = go {path = path, query = {page = 2}}
+            local posts = json.decode(table.concat(w.buffer))
+            assert.equals(2, #posts.items)
+            assert.equals(1, posts.paging.before)
+            assert.equals(3, posts.paging.after)
+        end)
+
+        it('supports query', function()
+            local w = go {path = path, query = {q = 'com'}}
+            local posts = json.decode(table.concat(w.buffer))
+            assert.equals(1, #posts.items)
+            assert.is_nil(posts.paging.before)
+            assert.is_nil(posts.paging.after)
+        end)
+    end)
+
+    describe('get post', function()
+        local path = path_for('post', {slug = 'inventore-hic-voluptatem'})
+
+    	it('responds with not found status code', function()
+            local w = go {path = path_for('post', {slug = 'unknown'})}
+            assert.equals(404, w.status_code)
+            assert.same({}, w.headers)
+        end)
+
+    	it('responds with a post', function()
+            local w = go {path = path}
+            assert.is_nil(w.status_code)
+            assert.same({['Content-Type'] = 'application/json'}, w.headers)
+            local p = json.decode(table.concat(w.buffer))
+            assert(p)
+            assert.is_nil(p.permissions)
+            assert.is_nil(p.comments)
+        end)
+
+    	it('returns post with permissions', function()
+            local w = go {
+                path = path,
+                query = {fields = 'permissions'}
+            }
+            local p = json.decode(table.concat(w.buffer))
+            assert(p.permissions)
+        end)
+
+    	it('returns post with comments', function()
+            local w = go {
+                path = path,
+                query = {fields = 'comments'}
+            }
+            local p = json.decode(table.concat(w.buffer))
+            assert(p.comments)
         end)
     end)
 end)
