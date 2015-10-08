e608034c9f7427cea6caaf3be20b233d522bfb9e
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-08 18:05:24 +0300

    Added auth cookie support to application.

--- src/app.lua
+++ src/app.lua
@@ -1,8 +1,31 @@
-local options = {}
+local encoding = require 'core.encoding'
+local cipher = require 'security.crypto.cipher'
+local digest = require 'security.crypto.digest'
+local principal = require 'security.principal'
+local ticket = require 'security.crypto.ticket'
+local http = require 'http'
+local web = require 'web'
+
+local _ = require 'shared.utils'
+
+
+local options = {
+    principal = principal,
+    ticket = ticket.new {
+        digest = digest.hmac('ripemd160', 'L6hc@wmA4xWCg6!NrTAGPd'),
+        cipher = cipher.new('aes256', 'jmENHqSzGz%SaCd&rAVPPk'),
+        encoder = encoding.new('base64')
+    }
+}
 
 local function new()
-    local web = require 'web'
-    local _ = require 'shared.utils'
+    local root_path = options.root_path or '/'
+    options.auth_cookie = {
+        name = '_a',
+        path = root_path,
+        -- TODO
+        deleted = http.cookie.delete {name = '_a', path = root_path}
+    }
     options.urls = _.load_urls {'membership', 'posts', 'public'}
     return web.app({web.middleware.routing}, options)
 end
