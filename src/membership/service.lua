8b8a8653c125f14da89b724168ad327a29d24592
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-08 11:49:42 +0300

    Added user method to membership service.

--- src/membership/service.lua
+++ src/membership/service.lua
@@ -1,2 +1,10 @@
+local MembershipService = {}
+
+
+function MembershipService:user()
+    return self.factory.membership:get_user(self.principal.id)
+end
+
 return {
+    membership = MembershipService
 }
