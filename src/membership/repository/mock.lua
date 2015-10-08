523603a114f673906a4aad9b5577ccfa139129cb
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-08 18:03:06 +0300

    Added authenticate and has account to membership mock repository.

--- src/membership/repository/mock.lua
+++ src/membership/repository/mock.lua
@@ -10,9 +10,26 @@ local function find_user_by_id(user_id)
     end)
 end
 
+function MembershipRepository:authenticate(username)
+    local u = _.first(samples.users, function(u)
+        return u.username == username
+    end)
+    return u and {id = u.id, password = u.password}
+end
+
 function MembershipRepository:get_user(user_id)
-    local u = find_user_by_id(user_id)
+    local u = find_user_by_id(tonumber(user_id))
     return u and {id = tostring(u.id), username = u.username}
+end
+
+function MembershipRepository:has_account(username)
+    return _.first(samples.users, function(u)
+        return u.username == username
+    end) ~= nil
+end
+
+function MembershipRepository:create_account(r)
+    return false
 end
 
 return {
