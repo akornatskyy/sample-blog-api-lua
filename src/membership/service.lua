46c83faa0113c623143bd6433e27f8d9396d820b
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-08 18:04:10 +0300

    Applied errors mixin  to membership service; added authenticate and create account.

--- src/membership/service.lua
+++ src/membership/service.lua
@@ -1,8 +1,36 @@
-local MembershipService = {}
+local mixin = require 'core.mixin'
+local shared = require 'shared.mixins'
+
+
+local MembershipService = mixin({}, shared.ErrorsMixin)
 
 
 function MembershipService:user()
     return self.factory.membership:get_user(self.principal.id)
+end
+
+function MembershipService:authenticate(c)
+    local up = self.factory.membership:authenticate(c.username:lower())
+    if not up or up.password ~= c.password then
+        return self:set_error(
+            'The username or password provided is incorrect.')
+    end
+    return self.factory.membership:get_user(up.id)
+end
+
+function MembershipService:create_account(r)
+    if self.factory.membership:has_account(r.username) then
+        return self:set_error(
+            'The user with such username is already registered. ' ..
+            'Please try another.',
+            'username')
+    end
+    if not self.factory.membership:create_account(r) then
+        return self:set_error(
+            'The system was unable to create an account for you. ' ..
+            'Please try again later.')
+    end
+    return true
 end
 
 return {
