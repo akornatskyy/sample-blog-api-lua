ff6b12f6024e26b14d313f5b05f4ec1ccc874bff
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-08 11:49:11 +0300

    Added find user by id to membership mock repository.

--- src/membership/repository/mock.lua
+++ src/membership/repository/mock.lua
@@ -1,7 +1,21 @@
 local _ = require 'shared.mock'
 
 local samples = _.load_samples()
-assert(samples)
+local MembershipRepository = {}
+
+
+local function find_user_by_id(user_id)
+    return _.first(samples.users, function(u)
+        return u.id == user_id
+    end)
+end
+
+function MembershipRepository:get_user(user_id)
+    local u = find_user_by_id(user_id)
+    return u and {id = tostring(u.id), username = u.username}
+end
 
 return {
+    membership = MembershipRepository,
+    find_user_by_id = find_user_by_id
 }
