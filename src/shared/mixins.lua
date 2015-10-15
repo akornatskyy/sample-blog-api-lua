26d8980e5d4cc1af33a195eda6a5ee8540d83c63
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-15 14:26:46 +0300

    Minor cleanup in errors mixin.

--- src/shared/mixins.lua
+++ src/shared/mixins.lua
@@ -2,10 +2,7 @@ local ErrorsMixin = {}
 
 
 function ErrorsMixin:set_error(msg, field)
-    if not field then
-        field = '__ERROR__'
-    end
-    self.errors[field] = msg
+    self.errors[field or '__ERROR__'] = msg
 end
 
 return {
