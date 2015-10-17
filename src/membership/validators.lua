84c72306b2f47586cb26b2713d3e18d5dcebb827
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-17 13:50:16 +0300

    Updated membership validators.

--- src/membership/validators.lua
+++ src/membership/validators.lua
@@ -1,4 +1,6 @@
 local validator = require 'validation.validator'
+local compare = require 'validation.rules.compare'
+local email = require 'validation.rules.email'
 local length = require 'validation.rules.length'
 local required = require 'validation.rules.required'
 
@@ -7,15 +9,18 @@ local rules = require 'membership.rules'
 
 return {
     credential = validator.new {
-        username = {required, length{min=2}, length{max=20}},
+        username = rules.username,
         password = rules.password
     },
 
     password_match = validator.new {
-        password = {}
+        password = {
+            compare{equal='confirm_password', msg='Passwords do not match.'}
+        }
     },
 
     registration = validator.new {
-        email = {required, length{min=6}, length{max=30}}
+        email = {required, length{min=6}, length{max=30}, email},
+        username = rules.username
     },
 }
