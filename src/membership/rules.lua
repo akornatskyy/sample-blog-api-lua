317f67385046ad9e970ed823fe51fe110690ee12
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-17 13:49:35 +0300

    Added username validation rule.

--- src/membership/rules.lua
+++ src/membership/rules.lua
@@ -3,5 +3,6 @@ local required = require 'validation.rules.required'
 
 
 return {
+    username = {required, length{min=2}, length{max=20}},
     password = {required, length{min=8}, length{max=12}}
 }
