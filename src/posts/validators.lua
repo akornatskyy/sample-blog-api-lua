4409c4ffb255b0438bc7bf357cfaab36aa0744bb
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-17 13:50:28 +0300

    Updated posts validators.

--- src/posts/validators.lua
+++ src/posts/validators.lua
@@ -1,5 +1,6 @@
 local validator = require 'validation.validator'
 local length = require 'validation.rules.length'
+local range = require 'validation.rules.range'
 local required = require 'validation.rules.required'
 
 local rules = require 'posts.rules'
@@ -8,12 +9,12 @@ local rules = require 'posts.rules'
 return {
     search_posts = validator.new {
         q = {length{max=20}},
-        page = {}
+        page = {range{min=0}, range{max=9}}
     },
 
     post_spec = validator.new {
         slug = rules.slug,
-        fields = {}
+        fields = {length{max=20}}
     },
 
     post_comment = validator.new {
