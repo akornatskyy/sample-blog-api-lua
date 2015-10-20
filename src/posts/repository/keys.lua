3d69b1bd49107398f9d151676b3314705a60a8f0
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-20 12:35:33 +0300

    Updated posts cache keys.

--- src/posts/repository/keys.lua
+++ src/posts/repository/keys.lua
@@ -1,9 +1,24 @@
 return {
     search_posts = function(q, page)
-        return 'p:search_posts:' .. q .. ':' .. page
+        return 'p:sepo:' .. q .. ':' .. page
+    end,
+
+    get_post_id = function(slug)
+        return 'p:gpid:' .. slug
     end,
 
     get_post = function(slug)
-        return 'p:get_post:' .. slug
+        return 'p:gepo:' .. slug
+    end,
+
+    list_comments = function(post_id, author_id)
+        if author_id then
+            return 'p:lico:' .. post_id .. ':' .. author_id
+        end
+        return 'p:lico:' .. post_id
+    end,
+
+    count_comments_awaiting_moderation = function(user_id)
+        return 'p:ccam:' .. user_id
     end
 }
