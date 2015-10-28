6f03383fe82cec28966064c441c1d2358cd0a9ab
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-28 22:06:21 +0200

    Applied public cache profile to daily quote handler.

--- src/public/views.lua
+++ src/public/views.lua
@@ -1,10 +1,12 @@
 local class = require 'core.class'
 
+local cache_profile = require 'public.profile'
 local shared = require 'shared.views'
 
 
 local DailyQuoteHandler = class(shared.BaseHandler, {
     get = function(self)
+        self.w.cache_profile = cache_profile.public
         return self:json(self:get_daily_quote())
     end,
 
