e876778e09a3aeef44b56e8b0c7884ca28084859
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-31 15:41:45 +0200

    Added caching middleware to app; updated config options with cache.

--- src/app.lua
+++ src/app.lua
@@ -6,10 +6,12 @@ local ticket = require 'security.crypto.ticket'
 local http = require 'http'
 local web = require 'web'
 
+local config = require 'config'
 local _ = require 'shared.utils'
 
 
 local options = {
+    cache = config.cache,
     principal = principal,
     ticket = ticket.new {
         digest = digest.hmac('ripemd160', 'L6hc@wmA4xWCg6!NrTAGPd'),
@@ -27,7 +29,7 @@ local function new()
         deleted = http.cookie.delete {name = '_a', path = root_path}
     }
     options.urls = _.load_urls {'membership', 'posts', 'public'}
-    return web.app({web.middleware.routing}, options)
+    return web.app({web.middleware.caching, web.middleware.routing}, options)
 end
 
 return {options = options, new = new}
